3937fc281e85cc1cd4dc7f5b77198b18e64892c9
dlalwn <159164991+dlalwn@users.noreply.github.com>
2024-07-11 17:38:01 +0900

    Revert "[2024/07/11] 카드 생성 기능 구현"

--- src/main/java/com/gamja/trello/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamja.trello.repository;
-
-import com.gamja.trello.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
