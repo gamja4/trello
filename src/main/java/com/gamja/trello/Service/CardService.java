f5dea65ce5c4763f4129833f2ad50bb4e7c7a5f8
dlalwn <annie45671@gmail.com>
2024-07-11 16:55:14 +0900

    update: card 생성 기능 추가

--- src/main/java/com/gamja/trello/Service/CardService.java
+++ src/main/java/com/gamja/trello/Service/CardService.java
@@ -2,8 +2,10 @@ package com.gamja.trello.Service;
 
 import com.gamja.trello.dto.CardRequestDto;
 import com.gamja.trello.dto.CardResponseDto;
+import com.gamja.trello.entity.Board;
 import com.gamja.trello.entity.Card;
 import com.gamja.trello.entity.Section;
+import com.gamja.trello.entity.User;
 import com.gamja.trello.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,15 +15,30 @@ import org.springframework.stereotype.Service;
 public class CardService {
 
     private final CardRepository cardRepository;
+//    private final BoardRepository boardRepository;
+//    private final SectionRepository sectionRepository;
 
-    public CardResponseDto createCard(Long sectionId, CardRequestDto requestDto/*, User user*/) {
+    public CardResponseDto createCard(Long boardId, Long sectionId, CardRequestDto requestDto, User user) {
+        Board board = findBoardById(boardId);
         Section section = findSectionById(sectionId);
-        Card card = new Card();
 
+        Card card = new Card(requestDto, board, section, user);
+        Card saveCard = cardRepository.save(card);
+
+        return new CardResponseDto(card);
+
+    }
+
+    private Board findBoardById(Long boardId) {
         return null;
+        // return boardRepository.findById(boardId).orElseThrow(() ->
+        //      new IllegalArgumentException("해당 보드를 찾을 수 없습니다.))
     }
 
     private Section findSectionById(Long sectionId) {
         return null;
+        // return sectionRepository.findById(sectionId).orElseThrow(() ->
+        //      new IllegalArgumentException("해당 컬럼을 찾을 수 없습니다.))
     }
+
 }
