f4ad9cf984b3c549e1bf6a9f34ae9fc91b24bf27
dlalwn <annie45671@gmail.com>
2024-07-11 19:30:28 +0900

    update: 필수 데이터(제목, 카드 상태) 예외처리 추가

--- src/main/java/com/gamja/trello/Service/CardService.java
+++ src/main/java/com/gamja/trello/Service/CardService.java
@@ -8,21 +8,25 @@ import com.gamja.trello.entity.User;
 import com.gamja.trello.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CardService {
     private final CardRepository cardRepository;
-//    private final SectionRespository sectionRespository;
 
     public CardResponseDto createCard(Long sectionId, CardRequestDto requestDto, User user) {
         Section section = findSectionById(sectionId);
-        Card card = new Card(requestDto, section, user);
 
+        if(requestDto.getTitle() == null || requestDto.getTitle().isEmpty() ||
+            requestDto.getStatus() == null || requestDto.getStatus().isEmpty()){
+            throw new IllegalArgumentException("제목과 카드 상태를 입력해주세요");
+        }
+
+        Card card = new Card(requestDto, section, user);
         Card saveCard = cardRepository.save(card);
 
         return new CardResponseDto(card);
-
     }
 
     private Section findSectionById(Long sectionId) {
@@ -30,4 +34,6 @@ public class CardService {
 //        return sectionRepository.findById(sectionId).orElse(() ->
 //                new IllegalArgumentException("해당 컬럼은 존재하지 않습니다."));
     }
+
+
 }
